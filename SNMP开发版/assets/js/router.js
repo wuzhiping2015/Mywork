395ff01e9d628781c758dfc260425116f642a339
wuzhiping2015 <376649868@qq.com>
2020-05-20 09:47:04 +0800

    清空

--- SNMP开发版/assets/js/router.js
+++
@@ -1,8 +0,0 @@
-function changeSrc1()
-{
-    document.getElementById("iframe1").setAttribute("src","1.html");
-}
-function changeSrc2()
-{
-    document.getElementById("iframe1").setAttribute("src","2.html");
-}
